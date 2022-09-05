35d4c0b453831780191ee659e79b029482cbeb99
Daphne <daphneruth304@gmail.com>
2022-09-05 10:26:34 +0300

    post requests

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'hello from the server', app: 'Natours' });
 });
+
+app.send('/', (req, res) => {
+  res.send('you can post to this endpoint');
+});
+
 const port = 3000;
 
 app.listen(port, () => {
