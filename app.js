b9ad05fe1707519ea7092291223261484e7ebd98
Daphne <daphneruth304@gmail.com>
2022-09-05 10:12:54 +0300

    fixed some bug

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
-
 app.get('/', (req, res) => {
   res.status(200).send('hello fro the server');
 });
+const port = 3000;
 
-app.listen(port),
-  () => {
-    console.log('listening on port `${port}`');
-  };
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
