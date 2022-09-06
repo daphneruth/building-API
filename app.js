8a22f644c72516d3e1f23488945bf7b36e69184b
Daphne <daphneruth304@gmail.com>
2022-09-06 13:13:19 +0300

    URL parameters

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
-  console.log(req.body);
+app.get('/api/v1/tours/id', (req, res) => {
+  console.log(req.params);
+
   res.status(200).json({
     status: 'success',
     result: tours.length,
