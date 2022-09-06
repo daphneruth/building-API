e2023c7dacab3a0d7154b0da78f885d45af854ab
Daphne <daphneruth304@gmail.com>
2022-09-06 12:20:17 +0300

    post request with express

--- app.js
+++ app.js
@@ -26,7 +26,10 @@ app.get('/api/v1/users', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {});
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.end(tours);
+});
 const port = 3000;
 
 app.listen(port, () => {
