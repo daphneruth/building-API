8e11517e6dbad030ccb05ebd8dd5a3a7d0d8c273
Daphne <daphneruth304@gmail.com>
2022-09-06 22:11:31 +0300

    updated

--- app.js
+++ app.js
@@ -147,13 +147,9 @@ const userRouter = express.Router();
 tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
 
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
