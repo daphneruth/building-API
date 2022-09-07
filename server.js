671dd9af94ecadbe8e8a57a1be23a8e4a95a4759
Daphne <daphneruth304@gmail.com>
2022-09-07 09:27:49 +0300

    updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
