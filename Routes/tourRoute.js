69bc57ca31378a6bc815d669327a8ae7360b6e83
Daphne <daphneruth304@gmail.com>
2022-09-07 05:46:49 +0300

    updated

--- Routes/tourRoute.js
+++ Routes/tourRoute.js
@@ -2,6 +2,82 @@ const express = require('express');
 
 const tourRouter = express.Router();
 
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestAt: req.requestTime,
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tour,
+    },
+  });
+};
+const createTour = (req, res) => {
+  //console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<updated tour here>',
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+
+  res.status(404).json({
+    status: 'fail',
+    data: null,
+  });
+};
+
 tourRouter.route('/').get(getAllTours).post(createTour);
 
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
