688b0f765ae71f4921cd115eec2ef487455f3731
Daphne <daphneruth304@gmail.com>
2022-09-07 09:21:18 +0300

    updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const morgan = require('morgan');
 const express = require('express');
 
 const tourRouter = require('./Routes/tourRoute');
@@ -7,6 +8,10 @@ const userRouter = require('./Routes/userRoute');
 const app = express();
 
 // MIDDLEWARES
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
