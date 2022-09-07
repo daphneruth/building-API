4cbb43691132e28547a5a10d1766d3351f287ec0
Daphne <daphneruth304@gmail.com>
2022-09-07 09:34:53 +0300

    fixed some bug

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
 exports.checkId = (req, res, next, val) => {
