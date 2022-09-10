1d6a720a7e5c0e99c93052fcf807541004b25b16
Daphne <daphneruth304@gmail.com>
2022-09-10 11:53:14 +0300

    updated

--- server.js
+++ server.js
@@ -17,6 +17,24 @@ mongoose
   })
   .then(() => console.log('DB connection is successful'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
