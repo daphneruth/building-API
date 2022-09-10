c34cdcc31befdce9caa6ceb36c81f2486367fef8
Daphne <daphneruth304@gmail.com>
2022-09-10 10:25:52 +0300

    updated

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection is successful'));
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
