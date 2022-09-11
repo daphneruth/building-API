cd617b3ad861911d1c816d0cfe610b491191f25b
Daphne <daphneruth304@gmail.com>
2022-09-11 11:22:58 +0300

    updated

--- server.js
+++ server.js
@@ -17,38 +17,7 @@ mongoose
   })
   .then(() => console.log('DB connection is successful'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  ratings: 4.7,
-  price: 497,
-});
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => console.log('Error'));
-
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
