5178ea7e70c9a60255dbe6b5b03b98ee00768b55
Daphne <daphneruth304@gmail.com>
2022-09-14 06:07:46 +0300

    filtering

--- controller/tourController.js
+++ controller/tourController.js
@@ -14,7 +14,12 @@ const Tour = require('./../model/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    const tours = await Tour.find(req.query);
     res.status(200).json({
       status: 'success',
       result: tours.length,
