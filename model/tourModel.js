7fa02c52b2ccdd08e79fe621bc2e9971db1364a6
Daphne <daphneruth304@gmail.com>
2022-09-16 18:12:43 +0300

    aggregation middleware

--- model/tourModel.js
+++ model/tourModel.js
@@ -96,6 +96,13 @@ tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
+// AGGREGATION
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
