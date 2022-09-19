7d6e0417c9d30572b5894217cfc909f2cfc146d9
Daphne <daphneruth304@gmail.com>
2022-09-19 08:52:14 +0300

    enums

--- models/tourModel.js
+++ models/tourModel.js
@@ -24,6 +24,10 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
 
     summary: {
